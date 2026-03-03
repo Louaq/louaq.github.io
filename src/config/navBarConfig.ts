95ed64b46d56260ac3ff7febc551dedd030926dc
YangYang <74131522+Louaq@users.noreply.github.com>
2026-03-03 11:20:19 +0800

    add

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -33,7 +33,7 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
     // 支持自定义导航栏链接,并且支持多级菜单
 	links.push({
 		name: "状态",
-		url: "http://status.louaq.com",
+		url: "https://louaq.hyperping.app",
 		external: true,
 		icon: "material-symbols:cloud-done-rounded",
 	  });
