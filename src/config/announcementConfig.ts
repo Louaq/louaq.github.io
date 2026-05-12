7c839d342ec8a47611fd35286b8a6f1ad5a8901e
YangYang <74131522+Louaq@users.noreply.github.com>
2026-05-12 11:05:41 +0800

    fix: update

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,7 +2,7 @@ import type { AnnouncementConfig } from "../types/config";
 
 export const announcementConfig: AnnouncementConfig = {
   title: "重要提醒",
-  content: "网站将于今晚 22:00-24:00 进行维护，期间可能无法访问。",
+  content: "ACM MM 2026审稿截至时间16 May 2026, 19:59 China Standard Time",
   closable: false, // 重要提醒不可关闭
   link: {
     enable: false, // 不需要链接
