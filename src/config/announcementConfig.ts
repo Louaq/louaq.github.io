bc0f181158a4087c3cd28879cf94a7bb947587f8
YangYang <74131522+Louaq@users.noreply.github.com>
2026-05-17 14:08:32 +0800

    fix: 修复已知问题

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,7 +2,7 @@ import type { AnnouncementConfig } from "../types/config";
 
 export const announcementConfig: AnnouncementConfig = {
   title: "重要提醒",
-  content: "ACM MM 2026审稿截至时间16 May 2026, 19:59 China Standard Time",
+  content: "ACM MM 2026 Rebuttal开始时间6月4日",
   closable: false, // 重要提醒不可关闭
   link: {
     enable: false, // 不需要链接
