ce65368b3c2aae0f97d83ef96319dab789c95188
YangYang <74131522+Louaq@users.noreply.github.com>
2026-05-28 21:02:31 +0800

    fix: 修改ACM MM Rebuttal时间

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,7 +2,7 @@ import type { AnnouncementConfig } from "../types/config";
 
 export const announcementConfig: AnnouncementConfig = {
   title: "重要提醒",
-  content: "ACM MM 2026 Rebuttal开始时间6月4日",
+  content: "ACM MM 2026 Rebuttal开始时间5月28日",
   closable: false, // 重要提醒不可关闭
   link: {
     enable: false, // 不需要链接
