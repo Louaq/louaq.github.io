b4e134236536b6da0ea0f928098e63eec9bfee6b
YangYang <74131522+Louaq@users.noreply.github.com>
2026-06-15 15:11:07 +0800

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -105,7 +105,6 @@ export default defineConfig({
 		}),
 		icon({
 			include: {
-				"preprocess: vitePreprocess(),": ["*"],
 				"fa6-brands": ["*"],
 				"fa6-regular": ["*"],
 				"fa6-solid": ["*"],
